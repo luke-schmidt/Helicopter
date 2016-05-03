f11ba15b699af3b410180bf6fddb28d228f07bc0
nguthrie93 <nattdawg93@gmail.com>
2016-05-03 10:42:40 -0400

    deleteed explosion code

--- www/main.js
+++ www/main.js
@@ -38,7 +38,6 @@ background.src = "images/underwater.jpg";
 var sharkX;
 var sharkY;
 var iterationCount;
-//var brickList;
 var mineList;
 var smokeList;
 var gameState;
@@ -47,6 +46,8 @@ var scrollVal;
 
 var ascentRate;
 var descentRate;
+
+ var particles = [];
 
 $(document).ready(function() {
     document.addEventListener("deviceready", onDeviceReady, false);
@@ -72,7 +73,6 @@ function setup() {
     
     shark.src = "images/shark.png";
 
-//    brickList = new Array();
     mineList = new Array();
     smokeList = new Array();
 
@@ -89,7 +89,6 @@ function setup() {
 
     ctx.font = font;
 
-//    addBrick();
     addMine();
 
     ctx.drawImage(background, 0, 0, backgroundWidth, backgroundHeight);
@@ -121,7 +120,6 @@ function draw() {
         clearScreen();
         animateBackground();
         animateShark();
-//        animateBricks();
         animateMines();
         ctx.font = "20px Bold Verdana";
         ctx.fillStyle = textColor;
@@ -134,7 +132,7 @@ function draw() {
 }
 
 function drawCrash() {    
-    shark.src = "chopper_burn.png";
+    shark.src = "images/shark-boom.png";
     ctx.drawImage(shark, sharkX, sharkY, sharkWidth, sharkHeight);
     
     ctx.fillstyle = "white";
@@ -168,109 +166,6 @@ function animateShark() {
             descentRate += gravity;
         }
     }
-    
-     /* Array of particles (global variable)
-*/
- var particles = [];
-
- 
-    function randomFloat (min, max)
-{
-	return min + Math.random()*(max-min);
-}
-    
-    function Particle ()
-{
-	this.scale = 1.0;
-	this.x = 0;
-	this.y = 0;
-	this.radius = 20;
-	this.color = "#000";
-	this.velocityX = 0;
-	this.velocityY = 0;
-	this.scaleSpeed = 0.5;
-
-	this.update = function(ms)
-	{
-		// shrinking
-		this.scale -= this.scaleSpeed * ms / 1000.0;
-
-		if (this.scale <= 0)
-		{
-			this.scale = 0;
-		}
-		// moving away from explosion center
-		this.x += this.velocityX * ms/1000.0;
-		this.y += this.velocityY * ms/1000.0;
-	};
-
-	this.draw = function(ctx)
-	{
-		// translating the 2D context to the particle coordinates
-		ctx.save();
-		ctx.translate(this.x, this.y);
-		ctx.scale(this.scale, this.scale);
-
-		// drawing a filled circle in the particle's local space
-		ctx.beginPath();
-		ctx.arc(0, 0, this.radius, 0, Math.PI*2, true);
-		ctx.closePath();
-
-		ctx.fillStyle = this.color;
-		ctx.fill();
-
-		ctx.restore();
-	};
-}
-
-    
-    function createExplosion(x, y, color)
-{
-	var minSize = 10;
-	var maxSize = 30;
-	var count = 10;
-	var minSpeed = 60.0;
-	var maxSpeed = 200.0;
-	var minScaleSpeed = 1.0;
-	var maxScaleSpeed = 4.0;
-
-	for (var angle=0; angle<360; angle += Math.round(360/count))
-	{
-		var particle = new Particle();
-
-		particle.x = x;
-		particle.y = y;
-
-		particle.radius = randomFloat(minSize, maxSize);
-
-		particle.color = color;
-
-		particle.scaleSpeed = randomFloat(minScaleSpeed, maxScaleSpeed);
-
-		var speed = randomFloat(minSpeed, maxSpeed);
-
-		particle.velocityX = speed * Math.cos(angle * Math.PI / 180.0);
-		particle.velocityY = speed * Math.sin(angle * Math.PI / 180.0);
-
-		particles.push(particle);
-	}
-}
-    
-    function update (frameDelay)
-	{
-		// draw a white background to clear canvas
-		ctx.fillStyle = "#FFF";
-		ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-		
-		// update and draw particles
-		for (var i=0; i<particles.length; i++)
-		{
-			var particle = particles[i];
-			
-			particle.update(frameDelay);
-			particle.draw(ctx);
-		}
-	}
     
 
     // border detection
@@ -337,9 +232,7 @@ function collisionCheck() {
         if (sharkX < (mineList[i].x + mineWidth) && (sharkX + sharkWidth) > mineList[i].x
                     && sharkY < (mineList[i].y + mineHeight) && (sharkY + sharkHeight) > mineList[i].y ) {
             gameOver();
-            createExplosion(x, y, "#FF0000");
-			
-			createExplosion(x, y, "#FF8518");
+            
         }
     }
 }
